dced2094d64742ff17e7a8baa33ed12d8c227ba7
miguel-castillo-861 <miguelynestorpinamar@gmail.com>
2026-09-10 15:35:08 -0300

    Boton de Inicio (Intento 2)

--- Assets/scripts/Menus.cs
+++ Assets/scripts/Menus.cs
@@ -1,16 +1,10 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Menus : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void Jugar()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene("Inicio");
     }
 }
