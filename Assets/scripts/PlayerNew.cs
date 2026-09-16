a510e21d0523d22142c7e6756027ed4e36148c42
Damián Conti <damiansoldaiso999@gmail.com>
2026-09-16 00:20:24 -0300

    vicky y dami agregado de gravedad

--- Assets/scripts/PlayerNew.cs
+++ Assets/scripts/PlayerNew.cs
@@ -10,7 +10,10 @@ public class PlayerNew : MonoBehaviour
     private PlayerInput playerInput;
     public CharacterController controller;
     private Vector2 input;
-    public float speed = 10f;
+    public float gravity = -20f;
+    public float verticalVelocity;
+    public float jumpHeight = 2f;
+    private bool jumpPressed;
 
 
 
@@ -18,13 +21,49 @@ public class PlayerNew : MonoBehaviour
     {
        rb = GetComponent<Rigidbody>();
        playerInput = GetComponent<PlayerInput>();
+       controller = GetComponent<CharacterController>();
     }
 
     
     void Update()
     {
+
+        bool isGrounded = controller.isGrounded;
+        if (isGrounded && verticalVelocity < 0)
+        {
+            verticalVelocity = -2f;
+
+        }
+
+        if (jumpPressed && isGrounded)
+        {
+            verticalVelocity = Mathf.Sqrt(jumpHeight * -2f * gravity);
+                jumpPressed = false;
+        
+        }
+
+        verticalVelocity += gravity * Time.deltaTime;
+
         input = playerInput.actions["Move"].ReadValue<Vector2>();
+        Vector3 move = new Vector3(input.x, 0f, input.y).normalized;
+        move.y = verticalVelocity / force;
+
+        controller.Move(move * force * Time.deltaTime);
+
     }
+    public void OnMove(InputValue value)
+    {
+        input = value.Get<Vector2>();   
+    }
+
+    public void OnJump(InputValue value)
+    {
+        if (value.isPressed)
+        {
+            jumpPressed = true;
+        }
+    }
+
     private void FixedUpdate()
     {
         rb.AddForce(new Vector3(input.x, 0f, input.y) * force);
